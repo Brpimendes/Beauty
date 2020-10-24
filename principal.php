4b53e96d84fcf10a0eb9bf160f797e87ef99aabc
Camila Moreira <camilaterezamoreira@yahoo.com.br>
2020-10-24 14:17:17 -0300

    foi criada a pagina principal onde criamos o menu lateral e o ccabecalho

---
+++ principal.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Principal</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header class="header-login">
+        <div class="header-login-menu">
+            <div class="logo">
+                <a href="/"><img src="img/logo.png" alt="logo salão Beauty"></a>
+            </div>
+
+            <div class="welcome">
+                <h1>Bem vindo!</h1>
+
+                <p>É bom ter você aqui!</p>
+            </div>
+        </div>
+    </header>
+    <main> 
+        <div class="menu-lateral">
+            <nav>
+                <ul> 
+                    <li> Meu cadastro</li>
+                    <li> Menu </li>
+                </ul>
+            </nav>
+        
+        </div>
+    </main>
+</body>
+</html>
